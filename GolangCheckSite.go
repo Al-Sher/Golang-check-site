e732224e533b037db4895abb94f00b616f72a0f7
Al-Sher <web-dizic@yandex.ru>
2018-04-21 22:39:23 +0300

    Added more options

--- GolangCheckSite.go
+++ GolangCheckSite.go
@@ -11,22 +11,27 @@ import (
     "strconv"
 )
 
-var timer int = 1
-var proxy string = ""
+type Settings struct {
+    timer int
+    proxy string
+    timeout int
+    file string
+    useragent string
+}
 
-func testSite(u string) bool {
+func testSite(u string, s Settings) bool {
     if u == "" {
         return false
     }
     client := http.Client{
-        Timeout: time.Duration(5 * time.Second),
+        Timeout: time.Duration(s.timeout) * time.Second,
     }
-    if proxy != "" {
-        proxyUrl, _ := url.Parse(proxy)
+    if s.proxy != "" {
+        proxyUrl, _ := url.Parse(s.proxy)
         client = http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(proxyUrl)}}
     }
     req, _ := http.NewRequest("GET", u, nil)
-    req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT x.y; Win64; x64; rv:10.0) Gecko/20100101 Firefox/10.0")
+    req.Header.Add("User-Agent", s.useragent)
     resp, err := client.Do(req)
     if err != nil {
         fmt.Println("error: ", err)
@@ -56,36 +61,60 @@ func getSitesFromFile(namefile string) []string {
     return test
 }
 
-func getParamsFromArgs() {
+func getParamsFromArgs(s Settings) Settings {
     if len(os.Args) > 1 {
         for i, _ := range os.Args {
             if os.Args[i] == "proxy" {
                 i++
-                proxy = os.Args[i]
+                s.proxy = os.Args[i]
+                fmt.Println("use proxy: ", os.Args[i])
             }
             if os.Args[i] == "timer" {
                 i++
-                timer, _ = strconv.Atoi(os.Args[i])
+                s.timer, _ = strconv.Atoi(os.Args[i])
+                fmt.Println("use timer: ", os.Args[i])
+            }
+            if os.Args[i] == "timeout" {
+                i++
+                s.timeout, _ = strconv.Atoi(os.Args[i])
+                fmt.Println("use timeout: ", os.Args[i])
+            }
+            if os.Args[i] == "file" {
+                i++
+                s.file = os.Args[i]
+                fmt.Println("use file: ", os.Args[i])
+            }
+            if os.Args[i] == "useragent" {
+                i++
+                s.useragent = os.Args[i]
+                fmt.Println("use useragent: ", os.Args[i])
             }
         }
     }
+    return s
+}
+
+func initSettings() Settings {
+    s := Settings{
+        timer: 1,
+        proxy: "",
+        timeout: 5,
+        file: "sites.txt",
+        useragent: "Mozilla/5.0 (Windows NT x.y; Win64; x64; rv:10.0) Gecko/20100101 Firefox/10.0",
+    }
+    return s
 }
 
 func main() {
-    sites := getSitesFromFile("sites.txt")
-    getParamsFromArgs()
-    if timer > 1 {
-        fmt.Println("use timer: ", timer)
-    }
-    if proxy != "" {
-        fmt.Println("use proxy: ", proxy)
-    }
+    s := initSettings()
+    s = getParamsFromArgs(s)
+    sites := getSitesFromFile(s.file)
     ch := make(chan byte, 1)
     for _, site := range sites {
         go func (site string) {
             for {
-                testSite(site)
-                time.Sleep(time.Duration(timer) * time.Second)
+                testSite(site, s)
+                time.Sleep(time.Duration(s.timer) * time.Second)
             }
             ch <- 1
         }(site)
